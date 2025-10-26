9d3c48b4945f398d798bb1fcd5c22e969671d9c8
Akhtar Khan <akhtarkhanak2712@gmail.com>
2025-10-26 16:19:43 +0530

    Adding Class Code

---
+++ clean_code/src/main/databases/mysql_conn_class.py
@@ -0,0 +1,67 @@
+import mysql.connector
+from loguru import logger
+
+
+class MySqlConnection:
+    def __init__(self, config):
+        self.config = config
+        self.connection = None
+
+
+    def mysql_db_connection(self):
+        try:
+            self.connection = mysql.connector.connect(
+                host = self.config["mysql_database"]["host"],  
+                user = self.config["mysql_database"]["user"],
+                password = self.config["mysql_database"]["password"],
+                database= self.config["mysql_database"]["database"] 
+            )
+            logger.info("Connection established successfully!")
+            return self.connection
+        except mysql.connector.Error as err:
+            logger.error(f"Error connecting to MySQL: {err}")
+            raise err
+
+
+    def closer(self):
+        if self.connection.is_connected():
+            self.connection.close()
+            logger.info("Connection Closed")
+
+
+class MySqlCRUDOperation:
+    def __init__(self, mysql_connection):
+        self.connection = mysql_connection
+
+
+    def read_from_mysql(self, query):
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            return rows
+        except Exception as e:
+            logger.error(f"Error in query: {e}")
+            raise e
+        finally:
+            if cursor:
+                cursor.close()
+                logger.info("Cursor Closed")
+
+
+    # Use Update, Insert and Delete Queries 
+    def insert_into_mysql(self, query):
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(query)
+        except Exception as e:
+            logger.error(f"Error in query: {e}")
+            raise e
+        finally:
+            if cursor:
+                self.connection.commit()
+                logger.info(f"Changes Commited")
+                cursor.close()
+                logger.info("Cursor Closed")
+
+
--- clean_code/src/main/main.py
+++ clean_code/src/main/main.py
@@ -1,5 +1,6 @@
 import configparser
 from databases.mysql_conn import * 
+from databases.mysql_conn_class import *
 from loguru import logger
 
 
@@ -8,9 +9,18 @@ config.read("resources/config.ini")
 
 
 def main():
-    q = "select * from information_schema.TABLES limit 5;"
-    read_from_mysql(config, q)
-    logger.info("Program Completed")
+    # Using Functional Approach
+    # q = "select * from information_schema.TABLES limit 5;"
+    # read_from_mysql(config, q)
+    # logger.info("Program Completed")
+
+    # Using Class Approach
+    mysql_db_conn = MySqlConnection(config)
+    curd_operation = MySqlCRUDOperation(mysql_db_conn.mysql_db_connection())
+    # ret = curd_operation.read_from_mysql("select * from info")
+    curd_operation.insert_into_mysql("Truncate table info")
+    # logger.info(f"{ret}")
+    mysql_db_conn.closer()
 
 
 if __name__ == "__main__":
